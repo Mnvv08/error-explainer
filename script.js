97be85ffa7c10b2797e5415580d302d358d210a0
Mnvv08 <pariharrmanavv@gmail.com>
2026-08-28 09:44:19 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// BugBlaster Scripts
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Add any interactive features here in the future
+    console.log("BugBlaster initialized! 🐛");
+});
